15bf18ad85c042782b31efa13d1f91d4544ad1a6
Emre Guneyler <Emre Guneyler>
2016-10-17 15:10:47 +0100

    base commit

---
+++ src/main/java/oyun/net/anagram/BaseView.java
@@ -0,0 +1,6 @@
+package oyun.net.anagram;
+
+public interface BaseView<T>
+{
+    void setPresenter(T presenter);
+}
---
+++ src/main/java/oyun/net/anagram/data/Word.java
@@ -0,0 +1,14 @@
+package oyun.net.anagram.data;
+
+public final class Word {
+{
+    private final String mWord;
+
+    public Word(String word) {
+        mWord = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
