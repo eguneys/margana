5455404a445a4a070edc000c1c734790e5027d39
Emre Guneyler <Emre Guneyler>
2016-10-19 08:58:20 +0100

    fab added

--- src/main/java/oyun/net/anagram/fragment/MainFragment.java
+++ src/main/java/oyun/net/anagram/fragment/MainFragment.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import oyun.net.anagram.R;
+
 public class MainFragment extends Fragment
 {
     public static MainFragment newInstance(boolean thing) {
@@ -21,5 +27,22 @@ public class MainFragment extends Fragment
         //         .commit();
         // }
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View contentView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // contentView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        //         @Override
+        //         public void onLayoutChange(View v, int left, int top, int right, int bottom,
+        //                                    int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        //             v.removeOnLayoutChangeListener(this);
+        //             setUpGridView(getView());
+        //         }
+        //     });
+        return contentView;
     }
 }
