cb5eb924fcc61a43e63b43b3408e164ea6a37a5e
Emre Guneyler <eguneys@hotmail.com>
2016-10-22 12:43:06 +0100

    letter view

---
+++ src/main/java/oyun/net/anagram/widget/LetterView.java
@@ -0,0 +1,52 @@
+package oyun.net.anagram.widget;
+
+import android.util.AttributeSet;
+import android.content.Context;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.animation.Interpolator;
+
+import android.graphics.Canvas;
+
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+
+import oyun.net.anagram.R;
+import oyun.net.anagram.widget.drawable.LetterDrawable;
+
+public class LetterView extends View {
+
+    private final Interpolator mLinearOutSlowInInterpolator = new LinearOutSlowInInterpolator();
+
+    private LetterDrawable mLetterDrawable;
+
+    public LetterView(Context context) {
+        super(context);
+        init();
+    }
+
+    public LetterView(Context context, AttributeSet attrs) {
+        super(context);
+        init();
+    }
+
+    public LetterView(Context context, AttributeSet attrs, int defStyleAttrs) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        mLetterDrawable = new LetterDrawable(R.color.theme_red_primary, R.color.theme_red_text);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        mLetterDrawable.setBounds(0, 0, w, h);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mLetterDrawable.draw(canvas);
+    }
+    
+}
---
+++ src/main/java/oyun/net/anagram/widget/drawable/LetterDrawable.java
@@ -0,0 +1,119 @@
+package oyun.net.anagram.widget.drawable;
+
+import android.util.Property;
+import android.util.Log;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.PixelFormat;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+import android.animation.ArgbEvaluator;
+
+public class LetterDrawable extends Drawable
+{
+    private final ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    private float mBackgroundColorProgress = 0f;
+
+    private int mColor;
+    private int mTextColor;
+
+    private Paint mPaint;
+    private Paint mTextPaint;
+
+    private Paint mAlphaPaint;
+
+    private Bitmap mTempBitmap;
+    private Canvas mTempCanvas;
+
+    private final RectF mBounds = new RectF();
+
+    public LetterDrawable(int bgColor, int textColor) {
+        mColor = bgColor;
+        mTextColor = textColor;
+        initPaints();
+    }
+
+    private void initPaints() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mAlphaPaint = new Paint(Paint.ANTI_ALIAS_FLAG |
+                                Paint.FILTER_BITMAP_FLAG |
+                                Paint.DITHER_FLAG);
+        
+
+        mPaint.setStyle(Paint.Style.FILL);
+        // mMaskPaint.setXfermode(new Por
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        updateBackgroundColor();
+    }
+
+    private void updateBackgroundColor() {
+        float colorProgress = mBackgroundColorProgress;
+        this.mPaint.setColor((Integer)argbEvaluator
+                             .evaluate(colorProgress, mColor, mTextColor));
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        int width = Math.abs(bounds.right - bounds.left);
+        int height = Math.abs(bounds.top - bounds.bottom);
+
+        super.onBoundsChange(bounds);
+        mBounds.left = bounds.left;
+        mBounds.right = bounds.right;
+        mBounds.top = bounds.top;
+        mBounds.bottom = bounds.bottom;
+
+        mTempBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mTempCanvas = new Canvas(mTempBitmap);
+    }
+
+    private void setupMode() {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+        mTempCanvas.drawOval(mBounds, mPaint);
+
+        canvas.drawBitmap(mTempBitmap, 0, 0, mAlphaPaint);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {}
+    @Override
+    public void setColorFilter(ColorFilter cf) {}
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+
+    public void setBackgroundColorProgress(float backgroundColorProgress) {
+        this.mBackgroundColorProgress = backgroundColorProgress;
+        updateBackgroundColor();
+    }
+    
+    public float getBackgroundColorProgress() {
+        return mBackgroundColorProgress;
+    }
+
+
+    public static final Property<LetterDrawable, Float> BACKGROUND_COLOR_PROGRESS =
+        new Property<LetterDrawable, Float>(Float.class, "backgroundColorProgress") {
+            @Override
+            public Float get(LetterDrawable object) {
+                return object.getBackgroundColorProgress();
+            }
+
+            @Override
+            public void set(LetterDrawable object, Float value) {
+                object.setBackgroundColorProgress(value);
+            }
+        };
+}
