d18d21d79fd5daf1e47c972cfa6f62ff4225bc54
Emre Guneyler <eguneys@hotmail.com>
2016-11-19 15:51:21 +0300

    dialog menu added

--- src/main/java/oyun/net/anagram/activity/HomeSelectionActivity.java
+++ src/main/java/oyun/net/anagram/activity/HomeSelectionActivity.java
@@ -1,11 +1,18 @@
 package oyun.net.anagram.activity;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.AsyncTask;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+
+import android.view.animation.Interpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
 
 import android.view.View;
 import android.widget.ImageView;
@@ -20,17 +27,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import oyun.net.anagram.R;
-import oyun.net.anagram.model.Category;
-import oyun.net.anagram.persistence.AnagramDatabaseHelper;
 
 import oyun.net.anagram.fragment.HomeSelectionFragment;
+
+import oyun.net.anagram.model.Category;
+import oyun.net.anagram.persistence.AnagramDatabaseHelper;
+import oyun.net.anagram.helper.ButtonTouchListener;
+
 
 public class HomeSelectionActivity extends AppCompatActivity
 {
 
     private static final int REQUEST_CATEGORY = 0x2300;
 
+    private final Interpolator DialogTranslateInterpolator = new AnticipateOvershootInterpolator();
+
     private View mNavigateMenu;
+
+    private View mBackgroundContainer;
+    private View mDialogContainer;
+    private View mMenuDialog;
+
+    private View mRateButton;
+    private View mFeedbackButton;
+
+    private boolean mIsDialogOpen;
+
+
+    private final View.OnClickListener mDialogOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()) {
+                case R.id.navigate_menu:
+                    setDialogState(true);
+                    break;
+                case R.id.dialog_container:
+                case R.id.dialog_close:
+                    setDialogState(false);
+                    break;
+                case R.id.feedback_button:
+                    onSendFeedback();
+                    break;
+                case R.id.rate_button:
+                    onRateApp();
+                    break;
+                default:
+                }
+            }
+        };
 
     public static void start(Context context) {
         Intent starter = getStartIntent(context);
@@ -62,9 +106,99 @@ public class HomeSelectionActivity extends AppCompatActivity
     }
 
     private void setUpToolbar() {
-        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_player);
-        // setSupportActionBar(toolbar);
+        mMenuDialog = findViewById(R.id.menu_dialog);
+
+        mMenuDialog.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {}
+            });
+
+        mDialogContainer = findViewById(R.id.dialog_container);
+        mDialogContainer.setOnClickListener(mDialogOnClickListener);
+
+        findViewById(R.id.dialog_close).setOnClickListener(mDialogOnClickListener);
+
+        mBackgroundContainer = findViewById(R.id.background_container);
         mNavigateMenu = (View) findViewById(R.id.navigate_menu);
+        mNavigateMenu.setOnClickListener(mDialogOnClickListener);
+
+        mRateButton = (View) findViewById(R.id.rate_button);
+        mRateButton.setOnTouchListener(new ButtonTouchListener(mRateButton));
+        mRateButton.setOnClickListener(mDialogOnClickListener);
+
+        mFeedbackButton = (View) findViewById(R.id.feedback_button);
+        mFeedbackButton.setOnTouchListener(new ButtonTouchListener(mFeedbackButton));
+        mFeedbackButton.setOnClickListener(mDialogOnClickListener);
+
+    }
+
+    private void setDialogState(boolean isOpen) {
+        if (mIsDialogOpen != isOpen) {
+            mIsDialogOpen = isOpen;
+
+            if (mIsDialogOpen) {
+                openDialogWithTransition();
+            } else {
+                closeDialogWithTransition();
+            }
+        }
+    }
+
+    private void closeDialogWithTransition() {
+        mBackgroundContainer
+            .animate()
+            .scaleX(1f)
+            .scaleY(1f)
+            .start();
+
+        mMenuDialog
+            .animate()
+            .translationY(-100)
+            .alpha(0)
+            .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        mMenuDialog.animate().setListener(null);
+                        mDialogContainer.setVisibility(View.GONE);
+                    }
+                })
+            .start();
+    }
+
+    private void openDialogWithTransition() {
+        mDialogContainer.setVisibility(View.VISIBLE);
+        mBackgroundContainer
+            .animate()
+            .scaleX(0.9f)
+            .scaleY(0.9f)
+            .start();
+
+        mMenuDialog.setTranslationY(-100);
+        mMenuDialog.setAlpha(0);
+        mMenuDialog
+            .animate()
+            .translationY(0)
+            .alpha(1)
+            .setInterpolator(DialogTranslateInterpolator)
+            .start();
+    }
+
+    private void onSendFeedback() {
+        Intent contactIntent = new Intent(Intent.ACTION_SENDTO,
+                                          Uri.fromParts("mailto",
+                                                        getString(R.string.email_to),
+                                                        null));
+        contactIntent.putExtra(Intent.EXTRA_SUBJECT,
+                               getString(R.string.email_subject));
+
+        startActivity(Intent.createChooser(contactIntent,
+                                           getString(R.string.email_chooser)));
+    }
+
+    private void onRateApp() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(myAppLinkToMarket);
     }
 
     private void attachMenuButtons() {
